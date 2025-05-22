36b4623f12bd3b7931e2a8dbf69f93117f3a4ba7
Ulugbek <mirzarakhmanoff@gmail.com>
2025-05-22 09:21:34 +0500

    done

--- lib/db.ts
+++ lib/db.ts
@@ -1,34 +1,37 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/edu-portal"
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/edu-portal";
 
-let cached = global.mongoose
+//@ts-expect-error
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = { conn: null, promise: null }
+  //@ts-expect-error
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
   if (cached.conn) {
-    return cached.conn
+    return cached.conn;
   }
 
   if (!cached.promise) {
     const opts = {
       bufferCommands: false,
-    }
+    };
 
     cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose
-    })
+      return mongoose;
+    });
   }
 
   try {
-    cached.conn = await cached.promise
+    cached.conn = await cached.promise;
   } catch (e) {
-    cached.promise = null
-    throw e
+    cached.promise = null;
+    throw e;
   }
 
-  return cached.conn
+  return cached.conn;
 }
